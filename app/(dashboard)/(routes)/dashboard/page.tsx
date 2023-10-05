0f8dcca438120300cd1e61aab74444443e9dec2d
GavPO <gavinporlich@gmail.com>
2023-10-05 00:18:02 -0500

    add sign in buttons

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Dashboard() {
     return (
-        <div>DASHBOARD (Protected)</div>
+        <>
+            <div>DASHBOARD (Protected)</div>
+            <UserButton />
+        </>
     )
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,18 @@
-export default function Landing(){
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export default function Landing() {
     return (
-        <div>
-            LANDING (Unprotected)
-        </div>
+        <>
+            <div>LANDING (Unprotected)</div>
+            <div className='flex flex-col' >
+                <Link href="/sign-in">
+                    <Button>LOGIN</Button>
+                </Link>
+                <Link href="sign-up">
+                    <Button>REGISTER</Button>
+                </Link>
+            </div>
+        </>
     )
 }
